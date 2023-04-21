79e648ae45a6904797dae66b1361b0de0d166d9c
annastass <86521173+annastass@users.noreply.github.com>
2023-04-21 20:37:28 +0400

    First commit

---
+++ RSAEncryption/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace RSAEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.publicKeyTextBox = new System.Windows.Forms.TextBox();
+            this.privateKeyTextBox = new System.Windows.Forms.TextBox();
+            this.generateKeysButton = new System.Windows.Forms.Button();
+            this.exportPublicKeyButton = new System.Windows.Forms.Button();
+            this.exportPrivateKeyButton = new System.Windows.Forms.Button();
+            this.ecryptButton = new System.Windows.Forms.Button();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.importPrivateKeyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // publicKeyTextBox
+            // 
+            this.publicKeyTextBox.Location = new System.Drawing.Point(12, 12);
+            this.publicKeyTextBox.Name = "publicKeyTextBox";
+            this.publicKeyTextBox.Size = new System.Drawing.Size(152, 22);
+            this.publicKeyTextBox.TabIndex = 1;
+            // 
+            // privateKeyTextBox
+            // 
+            this.privateKeyTextBox.Location = new System.Drawing.Point(12, 56);
+            this.privateKeyTextBox.Name = "privateKeyTextBox";
+            this.privateKeyTextBox.Size = new System.Drawing.Size(152, 22);
+            this.privateKeyTextBox.TabIndex = 2;
+            // 
+            // generateKeysButton
+            // 
+            this.generateKeysButton.Location = new System.Drawing.Point(205, 23);
+            this.generateKeysButton.Name = "generateKeysButton";
+            this.generateKeysButton.Size = new System.Drawing.Size(189, 38);
+            this.generateKeysButton.TabIndex = 3;
+            this.generateKeysButton.Text = "Сгенерировать ключ";
+            this.generateKeysButton.UseVisualStyleBackColor = true;
+            this.generateKeysButton.Click += new System.EventHandler(this.generateKeysButton_Click);
+            // 
+            // exportPublicKeyButton
+            // 
+            this.exportPublicKeyButton.Location = new System.Drawing.Point(207, 102);
+            this.exportPublicKeyButton.Name = "exportPublicKeyButton";
+            this.exportPublicKeyButton.Size = new System.Drawing.Size(196, 35);
+            this.exportPublicKeyButton.TabIndex = 4;
+            this.exportPublicKeyButton.Text = "Экспорт публ ключа";
+            this.exportPublicKeyButton.UseVisualStyleBackColor = true;
+            this.exportPublicKeyButton.Click += new System.EventHandler(this.exportPublicKeyButton_Click);
+            // 
+            // exportPrivateKeyButton
+            // 
+            this.exportPrivateKeyButton.Location = new System.Drawing.Point(12, 101);
+            this.exportPrivateKeyButton.Name = "exportPrivateKeyButton";
+            this.exportPrivateKeyButton.Size = new System.Drawing.Size(189, 36);
+            this.exportPrivateKeyButton.TabIndex = 6;
+            this.exportPrivateKeyButton.Text = "Экспорт прив ключа";
+            this.exportPrivateKeyButton.UseVisualStyleBackColor = true;
+            this.exportPrivateKeyButton.Click += new System.EventHandler(this.exportPrivateKeyButton_Click);
+            // 
+            // ecryptButton
+            // 
+            this.ecryptButton.Location = new System.Drawing.Point(12, 159);
+            this.ecryptButton.Name = "ecryptButton";
+            this.ecryptButton.Size = new System.Drawing.Size(189, 36);
+            this.ecryptButton.TabIndex = 8;
+            this.ecryptButton.Text = "Зашифровать";
+            this.ecryptButton.UseVisualStyleBackColor = true;
+            this.ecryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(207, 159);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(196, 36);
+            this.encryptButton.TabIndex = 9;
+            this.encryptButton.Text = "Дешифровать";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // importPrivateKeyButton
+            // 
+            this.importPrivateKeyButton.Location = new System.Drawing.Point(432, 102);
+            this.importPrivateKeyButton.Name = "importPrivateKeyButton";
+            this.importPrivateKeyButton.Size = new System.Drawing.Size(189, 36);
+            this.importPrivateKeyButton.TabIndex = 10;
+            this.importPrivateKeyButton.Text = "Импорт приватного";
+            this.importPrivateKeyButton.UseVisualStyleBackColor = true;
+            this.importPrivateKeyButton.Click += new System.EventHandler(this.importPrivateKeyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.importPrivateKeyButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.ecryptButton);
+            this.Controls.Add(this.exportPrivateKeyButton);
+            this.Controls.Add(this.exportPublicKeyButton);
+            this.Controls.Add(this.generateKeysButton);
+            this.Controls.Add(this.privateKeyTextBox);
+            this.Controls.Add(this.publicKeyTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox publicKeyTextBox;
+        private System.Windows.Forms.TextBox privateKeyTextBox;
+        private System.Windows.Forms.Button generateKeysButton;
+        private System.Windows.Forms.Button exportPublicKeyButton;
+        private System.Windows.Forms.Button exportPrivateKeyButton;
+        private System.Windows.Forms.Button ecryptButton;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button importPrivateKeyButton;
+    }
+}
+
---
+++ RSAEncryption/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RSAEncryption
+{
+    public partial class Form1 : Form
+    {
+        private RSACryptoServiceProvider rsa;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateKeysButton_Click(object sender, EventArgs e)
+        {
+            //генерация открытого и закрытого ключей
+            rsa = new RSACryptoServiceProvider();
+            publicKeyTextBox.Text = rsa.ToXmlString(false);
+            privateKeyTextBox.Text = rsa.ToXmlString(true);
+        }
+
+        private void exportPublicKeyButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "XML files (*.xml)|*.xml";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    writer.Write(publicKeyTextBox.Text);
+                }
+            }
+        }
+
+        private void exportPrivateKeyButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "XML files (*.xml)|*.xml";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    writer.Write(privateKeyTextBox.Text);
+                }
+            }
+        } 
+
+        private void importPrivateKeyButton_Click(object sender, EventArgs e)
+        {           
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML files (*.xml)|*.xml";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string xml = reader.ReadToEnd();
+                    rsa = new RSACryptoServiceProvider();
+                    rsa.FromXmlString(xml);
+                    privateKeyTextBox.Text = rsa.ToXmlString(true);
+                }
+            }
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //проверка, выбрал ли пользователь файл
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string inputFile = openFileDialog.FileName;
+                string outputFile = Path.Combine(Path.GetDirectoryName(inputFile), Path.GetFileNameWithoutExtension(inputFile) + "_encrypted" + Path.GetExtension(inputFile));
+
+                using (FileStream inputStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+                {
+                    using (FileStream outputStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+                    {
+                        int bufferSize = (int)Math.Ceiling(rsa.KeySize / 8.0) - 11; //вычисление размера буфера для зашифрованных данных
+                        byte[] buffer = new byte[bufferSize];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            byte[] encryptedData = rsa.Encrypt(buffer, false);
+                            outputStream.Write(encryptedData, 0, encryptedData.Length);
+                        }
+                        Application.DoEvents(); //исправяет зависание
+                    }
+                }
+
+                MessageBox.Show("Шифрование завершено");
+            }
+        }
+
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            //дешифрование файла данных с помощью закрытого ключа
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string inputFile = openFileDialog.FileName;
+                string outputFile = Path.Combine(Path.GetDirectoryName(inputFile), Path.GetFileNameWithoutExtension(inputFile) + "_decrypted" + Path.GetExtension(inputFile));
+                
+                using (FileStream fsInput = new FileStream(inputFile, FileMode.Open))
+                using (FileStream fsOutput = new FileStream(outputFile, FileMode.Create))
+                {
+                    byte[] buffer = new byte[rsa.KeySize / 8];
+                    int bytesRead;
+                    while ((bytesRead = fsInput.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        byte[] decryptedBuffer = rsa.Decrypt(buffer, false);
+                        fsOutput.Write(decryptedBuffer, 0, decryptedBuffer.Length);
+                    }
+
+                    Application.DoEvents(); //исправяет зависание
+                }
+                MessageBox.Show("Файл успешно дешифрован.");
+            }
+       }
+    }
+}
+
+
